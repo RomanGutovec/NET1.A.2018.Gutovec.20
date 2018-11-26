3ca1ac791a597563e3bbd6297d299a9e6294009b
Roman <Roman@ROMAN->
2018-11-26 11:22:54 +0300

    Task 4

--- No4.Solution.Console/Program.cs
+++ No4.Solution.Console/Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            RandomBytesFileGenerator bytesFileGenerator = new RandomBytesFileGenerator();
+            RandomCharsFileGenerator charsFileGenerator = new RandomCharsFileGenerator();
+            bytesFileGenerator.GenerateFiles(2, 15);
+            charsFileGenerator.GenerateFiles(3, 1);
         }
     }
 }
---
+++ No4.Solution/FileGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No4.Solution
+{
+    public abstract class FileGenerator
+    {
+        public string WorkingDirectory => "Files with random bytes";
+
+        public string FileExtension => ".bytes";
+
+        public void GenerateFiles(int filesCount, int contentLength)
+        {
+            for (var i = 0; i < filesCount; ++i)
+            {
+                var generatedFileContent = this.GenerateFileContent(contentLength);
+
+                var generatedFileName = $"{Guid.NewGuid()}{this.FileExtension}";
+
+                this.WriteBytesToFile(generatedFileName, generatedFileContent);
+            }
+        }
+
+        protected abstract byte[] GenerateFileContent(int contentLength);
+
+        protected abstract void WriteBytesToFile(string fileName, byte[] content);
+    }
+}
